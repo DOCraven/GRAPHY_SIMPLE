a5e2532904facf75fc610ea5380496dd3b5e1650
DOCraven <brg.tman@gmail.com>
2020-09-14 10:37:03 +1000

    added new global vars

--- config.py
+++ config.py
@@ -8,3 +8,11 @@ names = []
 chosen_site = ''
 Solar_Imported = False
 Solar_Exists = False
+
+Data_Uploaded = False
+
+#figures - yes this is bad juju but I need it to work 
+solar_figure_line = ''
+solar_figure_summed = ''
+yearly_summed_figure = ''
+
