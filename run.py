da4a421d5cb371623ed97aeb3f062992a1648b63
DOCraven <brg.tman@gmail.com>
2020-09-18 21:39:45 +1000

    moved all callbacks to run.py

--- run.py
+++ run.py
@@ -51,6 +51,183 @@ import config
 #   git push heroku HEAD:master
 ####################################################
 
+########## ACTUAL DASH LAYOUT GOES HERE ##########
+
+@app.callback(Output('tabs-example-content', 'children'),
+              [Input('tabs-example', 'value')])
+def render_content(tab):
+    if tab == 'tab-1': #LOAD DATA - PLACEHOLDER - TO BE BUILT
+        return html.Div([
+            html.H3('Load Interval and Solar Data'),
+            #upload data
+            html.P('Please upload consumption interval files and/or solar files\n. Please ensure the solar data file has the word "Solar" in it.'),
+            html.P('Please upload a maximum of 2 interval files'),
+            html.P('There is minimal error checking for number of data files uploaded. This may be added in future versions'),
+            html.P('Please be aware that the program takes a little while to do the analysis in the background. Currently there is no loading animation. This may change in future versions. Please be patient, this is a work in progress'),
+            dcc.Upload(
+                html.Button('Upload Files'), 
+                id='upload-data',
+                style={ #make a nice box around it
+                'width': '100%',
+                'height': '60px',
+                'lineHeight': '60px',
+                'borderWidth': '1px',
+                'borderStyle': 'dashed',
+                'borderRadius': '5px',
+                'textAlign': 'center',
+                'margin': '10px'
+            },
+            # Allow multiple files to be uploaded
+            multiple=True
+            ),
+            html.Div(id='output-data-upload'), #show the data, and this needs to exist for the code to work 
+
+
+        ])
+    
+    elif tab == 'tab-2': #FANCY INTERVAL GRAPHS
+        if config.Data_Uploaded: 
+            return html.Div([
+                html.H3('Historical Energy Load Anaylsis Tool'), #quick graph
+                dcc.Dropdown(id = 'Drop_Down_menu', #make selection menu
+                    options=[{'label':name, 'value':name} for name in config.Pricing_names],
+                    value = config.names[0],#initial default selection upon loading 
+                    multi=False #do not allow multiple selections 
+                    ), 
+                dcc.Graph(id='Pricing_daily_graph'), #display daily graph
+                dcc.Graph(id='Pricing_weekly_graph'), #display weekly graph
+                
+                
+            ])
+        else: 
+            return html.Div([
+                html.H3('Please upload interval and/or solar')
+            ])
+    
+    elif tab == 'tab-3': #LOAD SHIFTING SITE STUFF
+        if config.Data_Uploaded: 
+            if config.Solar_Exists: 
+                return html.Div([
+                    dcc.Store(id='memory_output'), #storing the selected site here
+                    dcc.Store(id='month_selection_output'), #storing the selected site here
+                    html.H3('Select a site to investigate'), 
+                    dcc.Dropdown(  #make drop down selection menu - STORING THIS BAD BOY IN THE DCC.STORE ABOVE
+                        id = 'Shifted_Drop_Down_menu', #unique identifier for DASH Callbacks
+                        options=[{'label':name, 'value':name} for name in config.names], #dynamically populating the list 
+                        value = config.names[0],#initial default selection upon loading 
+                        multi=False #do not allow multiple selections 
+                        ), 
+                    
+                    html.P(''), #blank row 
+                    html.H3('Select a % to load shift by'), 
+                    dcc.Slider( #load shifting slider thingo 
+                        id='shifting_slider', #unique identifier for DASH Callbacks
+                        min=0,
+                        max=50,
+                        step=1,
+                        value=0, #initial slider value - TODO: read dataframe holding that information in it 
+                    ),
+                    html.Div(id='slider-output-container'), #display slider output
+                    html.P(''), #blank row 
+                    html.H3('Select a Month to Plot'), 
+                    dcc.Dropdown(
+                        id='month_selection',
+                        options=[ #display options for dropdown value 
+                            {'label': 'January', 'value': 'January'},
+                            {'label': 'February', 'value': 'February'},
+                            {'label': 'March', 'value': 'March'},
+                            {'label': 'April', 'value': 'April'},
+                            {'label': 'May', 'value': 'May'},
+                            {'label': 'June', 'value': 'June'},
+                            {'label': 'July', 'value': 'July'},
+                            {'label': 'August', 'value': 'August'},
+                            {'label': 'September', 'value': 'September'},
+                            {'label': 'October', 'value': 'October'},
+                            {'label': 'November', 'value': 'November'},
+                            {'label': 'December', 'value': 'December'},
+                            ],
+                        value='January',
+                        multi=True
+                    ),
+                    
+                    dcc.Graph(id='shifting_slider_display'), #display the dynamically shifted graph upon update of slider 
+                    html.H4('Excess Solar'), #blank row 
+                    dcc.Graph(id='Daily Excess Summmed Solar - line ', figure = config.solar_figure_line), #display sum of all solar graph as a summed box per month
+                    ])
+            else: 
+                return html.H3("No Solar Data Uploaded")
+        else: 
+            return html.Div([
+                html.H3('Please upload interval and/or solar')
+            ]) 
+
+    elif tab == 'tab-4': #EXCESS SOLAR DATA 
+        if config.Data_Uploaded: 
+            if config.Solar_Exists: ## ie, user uploaded a solar file, so plot the nice and pretty graphs
+                return html.Div([
+                    html.H3("Solar Data Uploaded"),
+                    dcc.Graph(id='Daily Excess Summmed Solar - line ', figure = config.solar_figure_line), #display all excess solar graph as a line graph per month
+                    dcc.Graph(id='Daily Excess Summmed Solar - bar', figure = config.solar_figure_summed), #display sum of all solar graph as a summed box per month
+
+                ]) 
+            else: 
+                return html.H3("No Solar Data Uploaded")
+        
+        else: 
+            return html.Div([
+                html.H3('Please upload interval and/or solar')
+            ])
+
+    elif tab == 'tab-5': #EXPORT THE DATA TO A CSV - PLACEHOLDER - TO BE BUILT
+        if config.Data_Uploaded:
+                return html.Div([
+                    html.H3('Export Load Shift Data'),
+
+                ])
+        
+        else: 
+            return html.Div([
+                html.H3('Please upload interval and/or solar')
+            ])
+    
+    elif tab == 'tab-6': #YEARLY TOTALS PER SITE
+            if config.Data_Uploaded: 
+                return html.Div([
+                    html.H3('Yearly Site Consumption'),
+                    dcc.Graph(id='Total Yearly Site Consumption - bar', figure = config.yearly_summed_figure)
+
+                ])
+            else: 
+                return html.Div([
+                    html.H3('Please upload interval and/or solar')
+                ])
+
+
+    elif tab == 'tab-7': #Pricing Data
+            if config.Data_Uploaded: 
+                return html.Div([
+                    html.H3('Pricing Data'),
+                    dcc.Dropdown(id = 'Price_Drop_Down_menu', #make selection menu
+                        options=[{'label':name, 'value':name} for name in config.Pricing_names],
+                        value = config.Pricing_names[0],#initial default selection upon loading 
+                        multi=False #do not allow multiple selections 
+                        ), 
+                    dcc.Graph(id='Price_daily_graph'), #display daily graph
+                    dcc.Graph(id='Daily Excess Summmed Solar - line ', figure = config.solar_figure_line), #display all excess solar graph as a line graph per month
+                    dcc.Graph(id='Price_weekly_graph'), #display weekly graph
+                    ])
+            else: 
+                return html.Div([
+                    html.H3('Please upload interval and/or solar')
+                ])
+
+
+    elif tab == 'tab-99': #ABOUT - FILL IN WITH THE README WHEN I HAVE TIME
+            return html.Div([
+                html.H3('About'),
+                
+            ])
+
 
 
 
@@ -145,47 +322,6 @@ def filter_sites(value):
     [dash.dependencies.Input('month_selection', 'value')])
 def update_output(value):
     return (value) #ie, what is selected via the drop down box 
-
-######### CALLBACK FOR LOADING BUTTON ##########
-@app.callback(
-    
-    dash.dependencies.Output('container-button-basic', 'children'),
-    [dash.dependencies.Input('submit-val', 'n_clicks')],
-    [dash.dependencies.State('input-on-submit', 'value')]
-    )
-def update_output(n_clicks, value):
-    #open the load window on button click
-    sg.theme('Light Blue 2')
-
-    layout_landing = [[sg.Text('NEW Landing Page')], #layout
-            [sg.Text('Please open your interval data (and if required, solar data) in XLS format')],
-            [sg.Text('Interval Data', size=(10, 1)), sg.Input(), sg.FileBrowse()],
-            [sg.Text('Solar Data', size=(10, 1)), sg.Input(), sg.FileBrowse()],
-            [sg.Submit(), sg.Cancel()]]
-
-    if n_clicks >= 1:  #open the LOADING window only after a click
-        window = sg.Window('NEW Graphy (Simple)', layout_landing) #open the window 
-
-        event, values = window.read()
-        window.close()
-        config.Data_Uploaded = True
-        
-        try: #so I dont have to comment this out when automatically loading test data 
-            if event == 'Cancel': 
-                exit() #close the app
-        except NameError: 
-            pass 
-
-        #ensure someone has uploaded a file 
-        if not values[0]: #nothing uploaded
-            Mbox('UPLOAD ERROR', 'Please upload a CSV or XLSX file', 1) #spit out an error box 
-            exit() #close the app
-
-        Data_Analyser(values) #function to analyise all the data 
-
-        return 'Data loaded:  "{}" '.format(
-            values[0]
-        )
 
 ######### CALLBACK FOR DASH IMPORTING/LOADING FILES ##########
 @app.callback(Output('output-data-upload', 'children'),
